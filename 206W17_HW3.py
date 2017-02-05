a935fddbd93583049c450ff67b1e0fef2a2322c9
BJ Chatterson <wjchat@umich.edu>
2017-02-05 15:06:22 -0500

    Added section and time

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -3,7 +3,7 @@ import re
 
 ## SI 206 - W17 - HW3
 ## COMMENT WITH:
-## Your section day/time:
+## Your section day/time: Friday at 1 pm
 ## Any names of people you worked with on this assignment:
 
 #####################
